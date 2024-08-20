9a8aeec4bad26f403bc8053dd09be0e837a71a5e
Nathan <nathandong68@gmail.com>
2024-08-20 17:32:39 -0400

    ticket 1 - button added, working on function

--- src/app/supplies/page.tsx
+++ src/app/supplies/page.tsx
@@ -1,16 +1,27 @@
 import { getAll } from "@/server/db/actions/food";
 import Link from "next/link";
 import Row from "./Row";
+import { orderMoreFood } from "@/server/db/actions/food";
 
 export default async function Supplies() {
   const food = await getAll();
+
+  const orderAll = async () => {
+    const table = document.getElementById("foodTable") as HTMLTableElement;
+    for (let i = 0; i < table.rows.length;  i++) {
+      if (table.rows[i] ) {
+
+      }
+
+    }
+  };
 
   return (
     <main>
       <h1>Supplies</h1>
       <Link href="/">Back</Link>
 
-      <table>
+      <table id="foodTable">
         <thead>
           <tr>
             <td>Name</td>
@@ -24,6 +35,7 @@ export default async function Supplies() {
           ))}
         </tbody>
       </table>
+      <button>Order all foods in low quantity (&lt;5)</button>
     </main>
   );
 }
